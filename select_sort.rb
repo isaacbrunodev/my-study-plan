e80cbd3f37b6e89fd92393db800204cbebf84081
Isaac Bruno <isaacbruno1996@gmail.com>
2023-11-27 23:53:54 -0300

    select_sort explicado

--- select_sort.rb
+++ select_sort.rb
@@ -1,17 +1,20 @@
 def selection_sort(arr)
     n = arr.length
   
+    # Itera sobre todos os elementos do array, exceto o último
     (0...n-1).each do |i|
       min_index = i
   
+      # Encontra o índice do menor elemento no restante do array
       (i+1...n).each do |j|
         min_index = j if arr[j] < arr[min_index]
       end
   
-      # Troca os elementos
+      # Troca os elementos para colocar o menor no início
       arr[i], arr[min_index] = arr[min_index], arr[i] if i != min_index
     end
   
+    # Retorna o array ordenado
     arr
   end
   
